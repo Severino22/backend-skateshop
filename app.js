8b7b0b917210aa13823b5032374b14bd86d9abef
cristian riccieri <110061818+Severino22@users.noreply.github.com>
2023-04-12 21:17:09 +0200

    supresion de comentarios

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-//lo que se instala
+
 const express = require('express');
 const logger = require("morgan");
 const cors = require("cors")
 
-//importar ruta
+
 const indexRouter = require("./routes/index")
 const apiRouter = require("./routes/api")
 
-//connect
+
 const {connect} = require ("./db/db")
 
-//Express
+
 const app = express()
 
-//usos con express
+
 app.use(logger("dev"))
 app.use(express.json())
 app.use(cors())
 
-//rutas
+
 app.use("/", indexRouter)
 app.use("/api", apiRouter)
 
-//conexion con la db
+
 connect();
 
 
